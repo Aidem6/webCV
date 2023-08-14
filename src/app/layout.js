02c783dc96704585ae32cd5d55f05ff01debaed2
aidem6 <tomczyk006@gmail.com>
2023-08-14 15:40:24 +0200

    feature PWA

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,11 @@ const raleway = Raleway({ subsets: ['latin'] })
 export const metadata = {
   title: 'Adam Tomczyk CV',
   description: 'Find out more about Adam Tomczyk',
+  creator: 'Adam Tomczyk',
+  manifest: '/manifest.json',
+  themeColor: '#222831',
+  appleTouchIcon: '/logo192.png',
+  appleMobileWebAppStatusBar: '#222831',
 }
 
 export default function RootLayout({ children }) {
