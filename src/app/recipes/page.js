944cf0664e8fa2f3ad112fe3739675aadd2100eb
aidem6 <tomczyk006@gmail.com>
2023-08-14 20:21:24 +0200

    recipes?

---
+++ src/app/recipes/page.js
@@ -0,0 +1,10 @@
+export default function Docs() {
+  return (
+    <>
+      <h1>recipes</h1>
+      <p>
+        {"Who doesn't love food? I do! I'll try to share some of my favorite recipes here."}
+      </p>
+    </>
+  );
+}
