3123cc6179591ef2b39f6d5b8f600945b86ebf9b
aidem6 <tomczyk006@gmail.com>
2023-08-14 14:31:54 +0200

    fix education description

--- src/app/components/educationCard.js
+++ src/app/components/educationCard.js
@@ -9,15 +9,15 @@ export function EducationCard() {
       </h2>
       <InfoChunk
         additionalInfo='Poznań University of Technology'
-        date='2018 - 2022'
-        h3='Computer science - Bachelor degree'
+        date='2022 - today'
+        h3='Computer science - Masters | Internet of Things'
         description="Currently, I’m in the process of writing my master thesis. I’m researching best algorithms that can determine mobile user context based on accelerometer data."
       />
       <InfoChunk
         additionalInfo='Poznań University of Technology'
         date='2018 - 2022'
         h3='Computer science - Bachelor degree'
-        description="I’m proud that for our engineering diploma thesis, I with my team have received the 2nd degree distinction in the Engineer 4 Science 2022 competition under the patronage of the Polish Section of IEEE."
+        description="I’m proud that for our engineering diploma thesis, I along with my team have received the 2nd degree distinction in the Engineer 4 Science 2022 competition under the patronage of the Polish Section of IEEE."
       />
     </div>
   );
