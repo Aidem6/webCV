c0ac566b5f6c28b79c711b72f6172f4001251a03
aidem6 <tomczyk006@gmail.com>
2024-04-13 14:40:56 +0200

    fix warnings

--- src/app/docs/page.js
+++ src/app/docs/page.js
@@ -1,5 +1,4 @@
 'use client'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import styles from '../page.module.css';
 import '../globals.css';
