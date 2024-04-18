be01bf32f0da4bd1965e3154f0910b6789395189
aidem6 <tomczyk006@gmail.com>
2024-04-18 21:20:23 +0200

    remove motion in matzang page

--- src/app/maturazangielskiego/page.js
+++ src/app/maturazangielskiego/page.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import styles from '../page.module.css';
 import '../globals.css';
-import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faGooglePlay,
@@ -12,36 +11,6 @@ import {
 import Button from 'react-bootstrap/Button';
 
 export default function Docs() {
-  const [scrollDir, setScrollDir] = useState("up");
-
-  useEffect(() => {
-    const threshold = 0;
-    let lastScrollY = window.pageYOffset;
-    let ticking = false;
-
-    const updateScrollDir = () => {
-      const scrollY = window.pageYOffset;
-
-      if (Math.abs(scrollY - lastScrollY) < threshold) {
-        ticking = false;
-        return;
-      }
-      setScrollDir(scrollY > lastScrollY ? "down" : "up");
-      lastScrollY = scrollY > 0 ? scrollY : 0;
-      ticking = false;
-    };
-
-    const onScroll = () => {
-      if (!ticking) {
-        window.requestAnimationFrame(updateScrollDir);
-        ticking = true;
-      }
-    };
-    window.addEventListener("scroll", onScroll);
-
-    return () => window.removeEventListener("scroll", onScroll);
-  }, [scrollDir]);
-
   return (
     <Container className='w-100 h-100 overflow-hidden' style={{ backgroundColor: '#080808' }} >
       <Row>
